605430784cf25c69e313226106187190c4398d6d
Jordan Yates <yates.j.a@outlook.com>
2024-04-06 17:14:23 -0400

    backend

---
+++ src/backend/mod.rs
@@ -0,0 +1,26 @@
+// TODO: Make the backend
+
+/*
+```rust
+    struct Backend {
+        ...fields
+    }
+
+    struct Client {
+        ...fields
+    }
+
+    impl Backend {
+        pub fn new() -> Self {}
+
+        pub fn add_client(&mut self, client: Client) {}
+        // return a client that you can edit
+        pub fn edit_client(&mut self, client_id: u32) -> &mut Client {}
+        // Store the database into a file (hint: serde)
+        pub fn store(&self) {}
+        ...methods
+    }
+
+    ... whatever else you need
+```
+*/
